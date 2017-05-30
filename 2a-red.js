c90dbd9e2efe5a418e4f170a1e9b32441e52b8c1
Kyle Davelaar <kyle@Kyles-MacBook-Pro.local>
2017-05-30 13:44:52 -0700

    test for 20

---
+++ 2a-red.js
@@ -0,0 +1,35 @@
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+
+//Given a positive integer, return it as a string in Roman Numeral form.
+
+function roman(n, times) {
+  if (times === 1) {
+    return 'X'
+  }
+}
+
+
+test(10, 1, 'X');
+test(20, 2, 'XX');
+
+
+function test(n, times, actual) {
+  
+  describe('roman function', () => {
+    it('should return a string', () => {
+      assert.isString(roman(n, times), actual);
+    });
+    
+    it(`should return ${actual} for ${n}`, () => {
+      assert.equal(roman(n, times), actual);
+    })
+
+    it(`should have a string length of ${times}`, () => {
+      expect(roman(n, times).length).to.equal(times);
+    });
+  });
+
+}
+
+//TO TEST: $ mocha 2a-red
